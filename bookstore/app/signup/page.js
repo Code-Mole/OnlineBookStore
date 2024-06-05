ec580d08a44a031ee3022df58090c8a37689eec0
Asangabia Mole <moleasangabia23@gmail.com>
2024-06-05 14:20:07 +0000

    connecting the signUp page with the server

--- bookstore/app/signup/page.js
+++ bookstore/app/signup/page.js
@@ -1,11 +1,42 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "../login/login.module.css";
 import HomeIcon from "@mui/icons-material/Home";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import ContactsIcon from "@mui/icons-material/Contacts";
 import InfoIcon from "@mui/icons-material/Info";
+import axios from "axios";
 
 function page() {
+  const [first__name, setFirst__name] = useState("");
+  const [last__name, setLast__name] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const data = {
+    first__name,
+    last__name,
+    email,
+    password,
+  };
+
+  const registerUser = async () => {
+    await axios.post("http://localhost:8080/api/users", data).then((res) => {
+      setMessage(res.data);
+    });
+  };
+
+  const fetchUsers = async () => {
+    await axios.get("http://localhost:8080/api/users").then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [message]);
+
   return (
     <>
       <div className={styles.loginContainer}>
@@ -56,34 +87,39 @@ function page() {
                   type="text"
                   placeholder="First Name"
                   required
+                  onChange={(e) => setFirst__name(e.target.value)}
                 />
                 <input
                   id={styles.inputField}
                   type="text"
                   placeholder="Last Name"
                   required
+                  onChange={(e) => setLast__name(e.target.value)}
                 />
                 <input
                   id={styles.inputField}
                   type="email"
                   placeholder="Email"
                   required
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <input
                   id={styles.inputField}
                   type="password"
                   placeholder="Password"
                   required
-                />
-                <input
-                  id={styles.inputField}
-                  type="password"
-                  placeholder="Comfirm Password"
-                  required
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
 
-              <input id={styles.inputField1} type="submit" value="Sign Up" />
+              <input
+                id={styles.inputField1}
+                type="submit"
+                value="Register"
+                onClick={() => {
+                  registerUser();
+                }}
+              />
               <p id={styles.para1}>
                 I have an account? <a href="login">LogIn</a>
               </p>
